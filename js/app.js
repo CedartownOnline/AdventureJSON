7531cac3a04a6e4bf46fd7256c34c5752a745be0
jft5568 <james@cedartownonline.com>
2016-12-06 18:52:55 -0500

    ng-ENTER

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ var app = angular.module('App', []);
 app.controller('adventureController', function ($scope, $http) {
 
     var log = document.getElementById("log");
+    var commandBox = document.getElementById("command");
+    commandBox.focus();
     $scope.rooms = {};
     $scope.room = {};
+    $scope.command ="";
+
 
     $scope.Title = "Loading";
     $http.get("index.json")
@@ -15,12 +19,18 @@ app.controller('adventureController', function ($scope, $http) {
         });
 
 
+
+
+
     $scope.addLog = function (message) {
         var para = document.createElement("p");
-        var node = document.createTextNode(message);
-        para.appendChild(node);
-        log.appendChild(para);
+        para.innerHTML = message;
+        //var node = document.createTextNode(message);
+        //para.appendChild(node);
+        log.insertBefore(para, log.firstChild);
+        //log.appendChild(para);
     };
+
     $scope.setRoom = function (room, setRoom) {
 
         if ($scope.rooms[room]) {
@@ -42,7 +52,7 @@ app.controller('adventureController', function ($scope, $http) {
     };
 
     $scope.loadRoom = function (room, setCurrent) {
-        var url = "/rooms/" + room + ".json"
+        var url = "/rooms/" + room + ".json?U=" + guid();
         $http.get(url)
             .then(function (response) {
                 $scope.rooms[room] = response.data;
@@ -51,4 +61,43 @@ app.controller('adventureController', function ($scope, $http) {
             });
     };
 
+
+
+
+    $scope.processCommand = function(){
+        var cmd = $scope.command;
+        var cmdOK = false;
+
+        if (!cmdOK) $scope.addLog('<span class="message messageError">Unknown Command</span>' + cmd);
+        $scope.command ="";
+        commandBox.focus();
+    }
+
 });
+
+
+app.directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if(event.which === 13) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
+
+
+function guid() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+}
