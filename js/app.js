250cc039d7cd7fd2b320408f4d9e59110601dc0e
jft5568 <james@cedartownonline.com>
2016-12-07 18:44:32 -0500

    Command Processing

--- js/app.js
+++ js/app.js
@@ -1,86 +1,150 @@
 var app = angular.module('App', []);
-app.controller('adventureController', function ($scope, $http) {
+app.controller('adventureController', function ($scope, $http, $q) {
 
-    var log = document.getElementById("log");
-    var commandBox = document.getElementById("command");
-    commandBox.focus();
-    $scope.rooms = {};
-    $scope.room = {};
-    $scope.command ="";
-
-
-    $scope.Title = "Loading";
-    $http.get("index.json")
-        .then(function (response) {
-            $scope.indexJson = response.data;
-            $scope.Title = $scope.indexJson.title;
-            $scope.currentRoom = $scope.indexJson.start;
-            $scope.setRoom($scope.currentRoom);
-        });
-
-
-
-
-
-    $scope.addLog = function (message) {
-        var para = document.createElement("p");
-        para.innerHTML = message;
-        //var node = document.createTextNode(message);
-        //para.appendChild(node);
-        log.insertBefore(para, log.firstChild);
-        //log.appendChild(para);
-    };
-
-    $scope.setRoom = function (room, setRoom) {
-
-        if ($scope.rooms[room]) {
-            $scope.rooms[room].vistCount += 1;
-            $scope.room = $scope.rooms[room];
-        } else {
-            $scope.loadRoom(room, true);
-            return;
+        var log = document.getElementById("log");
+        var jLog = $('#log');
+        var titleDiv = $('#titleDiv');
+        var hColor = titleDiv.css('backgroundColor');
+        try {
+            document.styleSheets[0].addRule("#log::-webkit-scrollbar-thumb", "background-color: " + hColor + ";outline: 1px solid slategrey;");
+            console.log("Color:" + hColor);
+        } catch (err) {
+            console.log(err);
         }
 
-        if ($scope.room.vistCount>1){
-            $scope.addLog($scope.room.repeatVisit);
 
-        } else
-        {
-            $scope.addLog($scope.room.firstVisit);
-        }
-
-    };
-
-    $scope.loadRoom = function (room, setCurrent) {
-        var url = "/rooms/" + room + ".json?U=" + guid();
-        $http.get(url)
-            .then(function (response) {
-                $scope.rooms[room] = response.data;
-                $scope.rooms[room].vistCount = 0;
-                if (setCurrent) $scope.setRoom(room, true);
-            });
-    };
-
-
-
-
-    $scope.processCommand = function(){
-        var cmd = $scope.command;
-        var cmdOK = false;
-
-        if (!cmdOK) $scope.addLog('<span class="message messageError">Unknown Command</span>' + cmd);
-        $scope.command ="";
+        var commandBox = document.getElementById("command");
         commandBox.focus();
+        $scope.rooms = {};
+        $scope.room = {};
+
+        $scope.objects = {};
+        $scope.object = {};
+
+        $scope.command = "";
+
+
+        $scope.Title = "Loading";
+        $http.get("index.json")
+            .then(function (response) {
+                $scope.indexJson = response.data;
+                $scope.Title = $scope.indexJson.title;
+                $scope.currentRoom = $scope.indexJson.start;
+                $scope.setRoom($scope.currentRoom);
+            });
+
+
+        $scope.addLog = function (message) {
+            var para = document.createElement("p");
+            para.innerHTML = message;
+            log.appendChild(para);
+            //log.scrollTop = log.scrollHeight;
+
+            jLog.animate({scrollTop: jLog.prop("scrollHeight")}, 500);
+        };
+
+        $scope.setRoom = function (room) {
+
+            if (!$scope.rooms[room]) {
+                var url = "/rooms/" + room + ".json?U=" + guid();
+                $.ajax({
+                    url: url,
+                    async: false,
+                    dataType: 'json',
+                    success: function (response) {
+                        $scope.rooms[room] = response;
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
+            }
+            $scope.room = $scope.rooms[room];
+            if (!$scope.room.vistCount) $scope.room.vistCount = 0;
+            $scope.room.vistCount += 1;
+
+
+            if ($scope.room.vistCount > 1) {
+                $scope.addLog($scope.room.repeatVisit);
+
+            } else {
+                $scope.addLog($scope.room.firstVisit);
+            }
+            $scope.objects = {};
+
+            if ($scope.room.moves) {
+                $scope.room.moves.forEach(function (move, index) {
+                    if (move.object) {
+                        if (!move.object.content && move.object.id) {
+                            move.object.content = $scope.getObject(move.object.id);
+                        }
+
+                    }
+                });
+            }
+
+        };
+
+        $scope.getObject = function (objId) {
+            if (!$scope.objects[objId]) {
+                var url = "/objects/" + objId + ".json?U=" + guid();
+                $.ajax({
+                    url: url,
+                    async: false,
+                    dataType: 'json',
+                    success: function (response) {
+                        $scope.objects[objId] = response;
+                    }
+                });
+            }
+            return $scope.objects[objId];
+        };
+
+        $scope.processCommand = function () {
+
+            var cmd = $scope.command;
+            if (cmd.length < 1) return;
+            var cmdOK = false;
+
+            var clist = parseCommand(cmd, $scope);
+            if (clist.length > 0) {
+                cmdOK=true;
+                $scope.syntax = clist;
+            }
+            if (!cmdOK) $scope.addLog('<span class="message messageError">Unknown Command</span>' + cmd);
+            $scope.command = "";
+            commandBox.focus();
+        }
+
     }
+);
 
-});
+var commands = ["look", "move", "attack"];
+
+var parseCommand = function(command,scope){
+    var clist = command.match(/\S+/g) || [];
+    var syntaxList = [];
+    clist.forEach(function(word){
+        var cw=word.toLowerCase();
+        var cp = commands.indexOf(cw);
+        if (cp >= 0 ) {
+            var syntax = {}
+            syntax.word = word;
+            syntax.type = 1;
+            syntaxList.push(syntax);
+        }
 
 
-app.directive('ngEnter', function() {
-    return function(scope, element, attrs) {
-        element.bind("keydown keypress", function(event) {
-            if(event.which === 13) {
-                scope.$apply(function(){
+    });
+    return syntaxList;
+};
+
+
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if (event.which === 13) {
+                scope.$apply(function () {
                     scope.$eval(attrs.ngEnter);
                 });
 
